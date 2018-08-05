fe972eafe2bf0686eabde4961bc000f058b98823
David Ma <davidma415@gmail.com>
2018-08-05 00:55:12 -0400

    Finished new picture page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,4 +7,23 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
+  end
+
 end
