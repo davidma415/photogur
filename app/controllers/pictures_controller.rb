36fff67eb1580c22c9ac80ca3bdabb638cd37490
David Ma <davidma415@gmail.com>
2018-08-05 15:46:39 -0400

    Finished delete function

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -44,4 +44,10 @@ class PicturesController < ApplicationController
     end
   end
 
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
 end
